5b8d6c0b056c6be917886e335ce08f9f4f540884
nikohaatsi <nhaataja18@students.oamk.fi>
2026-04-08 11:26:48 +0300

    tallennus ja pisteidentarkistuksen alustusta

--- JS/common.js
+++ JS/common.js
@@ -1,1 +1,32 @@
-/**  SIvuston osien yhteiset js komponentit. Esim naviin/footeriin kuuuluvat ja pisteidenlaskenta. Tähän tiedostoon EI tehdä omia pelejä, eikä vain yhtä sivustoa koskevia asioita. */
+// Sivuston osien yhteiset js komponentit. Esim naviin/footeriin kuuuluvat ja pisteidenlaskenta. Tähän tiedostoon EI tehdä omia pelejä, eikä vain yhtä sivustoa koskevia asioita.
+
+
+//tallentaa pelien pisteet localstorageen
+//kutsukaa tätä peleissänne tallentaaksenne lopputulokset
+function tallenna(pelinimi, pisteet) {
+    localStorage.setitem(pelinimi, pisteet)
+
+
+
+}
+
+
+//(kesken)tarkistaa onko kaikki pelit pelattu tulosssivulle siirtymistä varten
+function haepisteet(){
+
+
+
+
+}
+
+
+
+
+//nollaa kaikki pisteet( kesken)
+
+
+function nollaaa() {
+
+
+
+}
