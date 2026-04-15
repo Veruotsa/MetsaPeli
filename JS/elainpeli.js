28b3f74d2c121ba7b856a215415218b9ad426bce
ottomatikainen <omatikai25@students.oamk.fi>
2026-04-15 14:17:50 +0300

    elainpelii muutoksia

--- JS/elainpeli.js
+++ JS/elainpeli.js
@@ -1,2 +1,17 @@
-// eläin luettelo
-{ name: "karhu"}
+// eläimet
+const animals = [
+    { name: "karhu", image: "karhu.png" },
+    { name: "ilves", image: "ilves.jpg" },
+    { name: "ahma", image: "ahma.jpg" },
+    { name: "susi", image: "susi.jpg" },
+    { name: "orava", image: "orava.jpg" },
+    { name: "kettu", image: "kettu2.jpg" }
+
+];
+
+shuffleArray(animals);
+
+let currentIndex = 0;
+let score = 0;
+let timeleft = 60;  
+
